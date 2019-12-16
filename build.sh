508159f73907023f1e6a180b2d468465c773e476
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-12-16 10:34:45 +0800

    u

--- build.sh
+++ build.sh
@@ -19,17 +19,20 @@ docker cp _output/$niche.yml rlang0:/root/_output.yml
 # dependency
 docker exec -w /root/repo/$wd rlang0 bash -c "$command"
 docker exec rlang0 bash -c "apt update && apt install -y $apt"
-docker exec -w /root/repo/$wd -e GITHUB_PAT=$GITHUB_PAT rlang0 Rscript -e "remotes::install_github('dongzhuoer/installr/$niche')"
+docker exec -e GITHUB_PAT=$GITHUB_PAT rlang0 Rscript -e "remotes::install_github('dongzhuoer/installr/$niche')"
+docker exec rlang0 Rscript -e ".packages(T)"
+# build book
 docker exec -w /root/repo/$wd rlang0 Rscript -e "bookdown::render_book('', zhuoerdown::make_gitbook('$root', '/root/_output.yml'), output_dir = '/root/gitbook')"
+# after script
 docker exec rlang0 Rscript -e "file.copy(zhuoerdown:::pkg_file('bookdown.css'), '/root/gitbook')"
-docker exec rlang0 chown -R `id -u`:`id -g` /usr/local/lib/R/site-library
-rm -r _bookdown_files && docker cp rlang0:/root/repo/$wd/_bookdown_files . 
-# wget https://gist.githubusercontent.com/dongzhuoer/c19d456cf8c1bd977a2f7916f61beee8/raw/cc-license.md
-# docker cp cc-license.md rlang0:/gitbook/readme.md
-
-#docker exec  rlang0 env
-
-# git clone --depth 1 $repo_git $work_dir/repo && mv $work_dir/repo/.git $work_dir/output
+docker exec rlang0 wget -o /root/gitbook/readme.md https://gist.githubusercontent.com/dongzhuoer/c19d456cf8c1bd977a2f7916f61beee8/raw/cc-license.md
+# deploy
+docker exec rlang0 git clone --depth 1 $repo_git $work_dir/repo && mv $work_dir/repo/.git $work_dir/output
 # cd $work_dir/output && git add --all && git commit -m "travis build at `date '+%Y-%m-%d %H:%M:%S'`" --allow-empty && git push -f
 
-# mv $work_dir/input/$wd/_bookdown_files $TRAVIS_BUILD_DIR || echo 'cache not available'
+# before cache
+docker exec rlang0 chown -R `id -u`:`id -g` /usr/local/lib/R/site-library
+rm -r _bookdown_files && docker cp rlang0:/root/repo/$wd/_bookdown_files . 
+
+
+
