43911efa6ccb67e9a786fe660bef82ef703add43
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-12-16 10:25:56 +0800

    bash -x

--- build.sh
+++ build.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 # build gitbook in Docker
+# @param repo, user/repo in GitHub, such as rstudio/bookdown-demo
+# @param wd, directory containing .Rmd files
+# @param url, website of original book
+# @param niche, ecological space including installr, dongzhuoer.bookdown.com, _output/*yml
+# @param apt, packages to install via APT
+# @param command, bash command to run at $wd
 
-# @details
-
-# @param 1 user/repo
-# @param 2 directory containing .Rmd files
-# @param 3 packages to install by APT
-# @param 4 bash command to run
-# @param 5 subdirectory dependent
-# @param 6 website of original book
-
-
-# @note
-docker rm -f rlang0
+    # docker rm -f rlang0
+# create container
 docker pull dongzhuoer/rlang:zhuoerdown 2> /dev/null
 docker run -dt --name rlang0 -v $HOME/.local/lib/R:/usr/local/lib/R/site-library dongzhuoer/rlang:zhuoerdown
-docker exec rlang0 bash -c "apt update && apt install -y $apt"
+# prepare files
 docker exec rlang0 git clone --depth 1 https://github.com/$repo.git /root/repo
     # mkdir -p _bookdown_files
 docker cp _bookdown_files rlang0:/root/repo/$wd 
 docker cp _output/$niche.yml rlang0:/root/_output.yml
+# dependency
 docker exec -w /root/repo/$wd rlang0 bash -c "$command"
+docker exec rlang0 bash -c "apt update && apt install -y $apt"
 docker exec -w /root/repo/$wd -e GITHUB_PAT=$GITHUB_PAT rlang0 Rscript -e "remotes::install_github('dongzhuoer/installr/$niche')"
 docker exec -w /root/repo/$wd rlang0 Rscript -e "bookdown::render_book('', zhuoerdown::make_gitbook('$root', '/root/_output.yml'), output_dir = '/root/gitbook')"
 docker exec rlang0 Rscript -e "file.copy(zhuoerdown:::pkg_file('bookdown.css'), '/root/gitbook')"
