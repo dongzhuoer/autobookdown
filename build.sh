1e3c374f0846061bfa5bf665f5f9482a3ea30284
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-12-21 23:23:42 +0800

    u

--- build.sh
+++ build.sh
@@ -6,15 +6,17 @@
 # @param niche, ecological space including installr, dongzhuoer.bookdown.com, _output/*yml
 # @param apt, packages to install via APT
 
-# prepare files
-mkdir -p wd
-git clone --depth 1 https://github.com/$repo.git wd/repo
-cp _output/$niche.yml wd/_output.yml
 
 # create container
 docker pull dongzhuoer/rlang:zhuoerdown > /dev/null
     # docker rm -f rlang0
-docker run -dt --name rlang0 -v `pwd`/wd:/root -v `pwd`/_bookdown_files:/root/repo/_bookdown_files -v $HOME/.local/lib/R/site-library:/usr/local/lib/R/site-library dongzhuoer/rlang:zhuoerdown
+docker run -dt --name rlang0 -v $HOME/.local/lib/R/site-library:/usr/local/lib/R/site-library dongzhuoer/rlang:zhuoerdown
+
+# prepare files
+docker exec rlang0 rm -r /root
+docker exec rlang0 git clone --depth 1 https://github.com/$repo.git /root
+docker cp _output/$niche.yml rlang0:/_output.yml
+docker cp _bookdown_files rlang0:/root
 
 # dependency
 docker exec rlang0 bash -c 'echo -e "[user]\n\tname = Zhuoer Dong\n\temail = dongzhuoer@mail.nankai.edu.cn\n" > /root/.gitconfig'
@@ -23,15 +25,20 @@ docker exec -e GITHUB_PAT=$GITHUB_PAT rlang0 Rscript -e "remotes::install_github
 
 # build book
 download_link="https://github.com/dongzhuoer/bookdown.dongzhuoer.com/archive/$niche.zip"
-docker exec -w /root/repo/$rmd rlang0 Rscript -e "bookdown::render_book('', zhuoerdown::make_gitbook('/root/_output.yml', '$url', '$download_link'), output_dir = '/root/output')"
-docker exec rlang0 Rscript -e "file.copy(zhuoerdown:::pkg_file('bookdown.css'), '/root/output')"
+docker exec -w /root/$rmd rlang0 Rscript -e "bookdown::render_book('', zhuoerdown::make_gitbook('/_output.yml', '$url', '$download_link'), output_dir = '/output')"
+docker exec rlang0 Rscript -e "file.copy(zhuoerdown:::pkg_file('bookdown.css'), '/output')"
 docker exec rlang0 bash -c "apt install -y wget"
-docker exec rlang0 wget -O /root/output/readme.md https://gist.githubusercontent.com/dongzhuoer/c19d456cf8c1bd977a2f7916f61beee8/raw/cc-license.md
-test -f wd/output/index.html || exit 1
-
-# before cache
-docker exec rlang0 chown -R `id -u`:`id -g` /root /usr/local/lib/R/site-library
+docker exec rlang0 wget -O /output/readme.md https://gist.githubusercontent.com/dongzhuoer/c19d456cf8c1bd977a2f7916f61beee8/raw/cc-license.md
+docker exec rlang0 test -f /output/index.html
 
 # deploy
-git clone --depth 1 -b $niche https://$GITHUB_PAT@github.com/dongzhuoer/bookdown.dongzhuoer.com.git wd/git && mv wd/git/.git wd/output && rm -r wd/git
-cd wd/output && git add --all && commit -m "Travis build at `date '+%Y-%m-%d %H:%M:%S'`" --allow-empty && git push && cd ../..
+docker exec rlang0 git clone --depth 1 -b $niche https://$GITHUB_PAT@github.com/dongzhuoer/bookdown.dongzhuoer.com.git /git 
+docker exec rlang0 mv /git/.git /output
+docker exec rlang0 rm -rf /git
+docker exec -w /output rlang0 git add --all
+docker exec -w /output rlang0 git commit -m "Travis build at `date '+%Y-%m-%d %H:%M:%S'`" --allow-empty 
+docker exec -w /output rlang0 git push
+
+# before cache
+docker exec rlang0 chown -R `id -u`:`id -g` /usr/local/lib/R/site-library
+rm -rf _bookdown_files && docker cp rlang0:/root/_bookdown_files .
